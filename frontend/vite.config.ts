4e8121a8f29bc78a26328c476d053d7632426d9b
Tevž Starovasnik <tevzstarovasnik@Tevzs-MacBook-Pro-2.local>
2025-11-28 22:56:44 +0100

    Fix: CI test v3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
-  test: {
-    environment: "jsdom",
-    setupFiles: "./vitest.setup.ts",
-    globals: true,
-  },
 });
